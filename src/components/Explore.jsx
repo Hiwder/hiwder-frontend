437852386940b03570db90512f54ef9554c1dfd6
Sirapatch Thammaleelakul <sirapetch@Sirapatchs-MacBook-Air.local>
2022-10-24 21:14:14 +0700

    💆‍♂️: Explore page edited lmao

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -9,19 +9,59 @@ const Explore = () => {
 
 	return (
 		<div>
-			<div className="container">
-				<div className="header">
-					<Link to="/">
-						<IconButton>
-							<ArrowsBackIosIcon fontSize="medium" />
-						</IconButton>
-					</Link>
-					<input
-						type="text"
-						placeholder="Search"
-						value={search}
-						onChange={(e) => setSearch(e.target.value)}
-					/>
+			<div className="header">
+				<Link to="/">
+					<IconButton>
+						<ArrowsBackIosIcon fontSize="medium" />
+					</IconButton>
+				</Link>
+				<input
+					className='searchbar'
+					type="text"
+					placeholder="Search"
+					value={search}
+					onChange={(e) => setSearch(e.target.value)}
+				/>
+			</div>
+			<div className='categories'>
+				<div className='category'>
+					<p>Thai Food</p>
+					<img src="./img/category1.png" />
+				</div>
+				<div className='category'>
+					<p>Dessert</p>
+					<img src="./img/category2.png" />
+				</div>
+				<div className='category'>
+					<p>Steak</p>
+					<img src="./img/category3.png" />
+				</div>
+			</div>
+			<div className='nearyou'>
+				<p style={{gridArea: 'header'}}>near you</p>
+				<div className='shop' style={{
+					gridArea: 'menu1'
+				}}>
+					<p>ร้านป้าเบลอ</p>
+					<img src="./img/nearyou1.png" />
+				</div>
+				<div className='shop' style={{
+					gridArea: 'menu2'
+				}}>
+					<p>ลุงเหนอ ก๋วยเตี๋ยวทรงเครื่อง</p>
+					<img src="./img/nearyou2.png" />
+				</div>
+				<div className='shop' style={{
+					gridArea: 'menu3'
+				}}>
+					<p>café amazon</p>
+					<img src="./img/nearyou3.png" />
+				</div>
+				<div className='shop' style={{
+					gridArea: 'menu4'
+				}}>
+					<p>Inthanin Coffee</p>
+					<img src="./img/nearyou4.png" />
 				</div>
 			</div>
 		</div>
